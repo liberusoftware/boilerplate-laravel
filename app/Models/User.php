4fad7b638e693bd354b098b1304a570339ce4bb5
Curtis Delicata <curtis.delicata@outlook.com>
2024-06-29 15:20:47 +0000

    Configure Jetstream classes in providers and separate panels into Admin and App

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,6 +10,7 @@ use JoelButcher\Socialstream\HasConnectedAccounts;
 use JoelButcher\Socialstream\SetsProfilePhotoFromUrl;
 use Laravel\Fortify\TwoFactorAuthenticatable;
 use Laravel\Jetstream\HasProfilePhoto;
+use Laravel\Jetstream\HasTeams;
 use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
 
