e0c54504c810e3262d957d79d0395d98dc218f4a
Curtis Delicata <curtis.delicata@outlook.com>
2024-07-01 01:44:36 +0000

    Configure new App Panel

--- config/app.php
+++ config/app.php
@@ -170,6 +170,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\Filament\AdminPanelProvider::class,
+        App\Providers\Filament\AppPanelProvider::class,
         App\Providers\RouteServiceProvider::class,
     ])->toArray(),
 
