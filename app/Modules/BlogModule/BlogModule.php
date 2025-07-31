d5c9b7acbd089a21b81c813ed1eafc2f9d7bebdd
johven <johven1984@hotmail.com>
2025-07-31 12:51:43 +0100

    Upgrade to Filament 4 Beta

--- app/Modules/BlogModule/BlogModule.php
+++ app/Modules/BlogModule/BlogModule.php
@@ -2,6 +2,7 @@
 
 namespace App\Modules\BlogModule;
 
+use Log;
 use App\Modules\BaseModule;
 
 class BlogModule extends BaseModule
@@ -9,24 +10,24 @@ class BlogModule extends BaseModule
     protected function onEnable(): void
     {
         // Called when module is enabled
-        \Log::info('Blog module has been enabled');
+        Log::info('Blog module has been enabled');
     }
 
     protected function onDisable(): void
     {
         // Called when module is disabled
-        \Log::info('Blog module has been disabled');
+        Log::info('Blog module has been disabled');
     }
 
     protected function onInstall(): void
     {
         // Called when module is installed
-        \Log::info('Blog module has been installed');
+        Log::info('Blog module has been installed');
     }
 
     protected function onUninstall(): void
     {
         // Called when module is uninstalled
-        \Log::info('Blog module has been uninstalled');
+        Log::info('Blog module has been uninstalled');
     }
 }
