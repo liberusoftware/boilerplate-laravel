3f8ac701918c529dfc389d41ecd42f54852a01d6
Joshua Kisb <joshuakisb@gmail.com>
2026-02-10 08:47:02 +0300

    fix styles and theme

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,34 @@
-import { defineConfig } from 'vite'
-import laravel, { refreshPaths } from 'laravel-vite-plugin'
+import { defineConfig } from "vite";
+import laravel, { refreshPaths } from "laravel-vite-plugin";
 import { viteStaticCopy } from "vite-plugin-static-copy";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js',
-		    'resources/css/filament/admin/theme.css'],
-            refresh: [
-                ...refreshPaths,
-                'app/Filament/**',
-                'app/Forms/Components/**',
-                'app/Livewire/**',
-                'app/Infolists/Components/**',
-                'app/Providers/Filament/**',
-                'app/Tables/Columns/**',
-            ],
-        }),
-        viteStaticCopy({
-            targets: [
-                {
-                    src: "resources/images/*",
-                    dest: "images",
-                },
-            ],
-        }),
-    ],
-})
+	plugins: [
+		laravel({
+			input: [
+				"resources/css/app.css",
+				"resources/js/app.js",
+				"resources/css/filament/admin/theme.css",
+			],
+			refresh: [
+				...refreshPaths,
+				"app/Filament/**",
+				"app/Forms/Components/**",
+				"app/Livewire/**",
+				"app/Infolists/Components/**",
+				"app/Providers/Filament/**",
+				"app/Tables/Columns/**",
+			],
+		}),
+		viteStaticCopy({
+			targets: [
+				{
+					src: "resources/images/*",
+					dest: "images",
+				},
+			],
+		}),
+		tailwindcss(),
+	],
+});
