2e675a0f5f860b9db52c5b965769104df280b39a
Curtis Delicata <curtis.delicata@outlook.com>
2026-03-20 14:21:22 +0000

    Refactor UserSeeder to create admin user
    Updated UserSeeder to create an admin user with a random password and assign a role. Removed staff password generation.

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,25 +2,36 @@
 
 namespace Database\Seeders;
 
+use App\Models\Role;
+use App\Models\Team;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
-     *
-     * @return void
+     * Run the database seeds.
      */
-    public function run()
+    public function run(): void
     {
+
         $adminPassword = Str::random(12);
-        $staffPassword = Str::random(12);
+        $adminUser = User::create([
+            'name' => 'Admin User',
+            'email' => 'admin@example.com',
+            'password' => Hash::make($adminPassword),
+            'email_verified_at' => now(),
+        ]);
+
+        $team = Team::firstOrFail();
+        $adminUser->teams()->syncWithoutDetaching([$team->id]);
+
+        $role = Role::where('name', 'super_admin')->firstOrFail();
+        $adminUser->assignRole($role);
 
         // Print passwords to console
         echo "Admin password: {$adminPassword}\n";
-        echo "Staff password: {$staffPassword}\n";
-
-        // Here you can save these passwords to your user creation logic as needed
     }
 }
